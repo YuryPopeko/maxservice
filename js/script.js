65cc1f19800971bf897c34d0beec637f8c44acb8
Yury Popeko <yurypopeko@gmail.com>
2018-08-15 17:28:33 +0300

    map

--- js/script.js
+++ js/script.js
@@ -11,6 +11,8 @@ document.addEventListener('click', (e) => {
 
 	if (target.closest('.hamburger')) toggleHeaderMenu()
 
+	if (target.closest('.scheme__item')) toggleScheme(target.closest('.scheme__item'))
+
 });
 
 function toggleTab(button) {
@@ -54,6 +56,43 @@ function closeModal() {
 function toggleHeaderMenu() {
 
 	document.querySelector('.header__menu').classList.toggle('open')
+
+}
+
+function toggleScheme(elem) {
+
+	if (elem.classList.contains('scheme__item_active')) return;
+
+	const container = elem.parentElement;
+	container.querySelector('.scheme__item_active').classList.remove('scheme__item_active');
+
+	elem.classList.add('scheme__item_active');
+
+}
+
+
+var brest = {lat: 52.09762139999999, lng: 23.734050300000035},
+		minsk = {lat: 53.90453979999999, lng: 27.561524400000053};
+
+function initMap() {
+
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: brest,
+		zoom: 12
+	});
+
+	document.querySelector('.map__toggle').addEventListener('click', function(e) {
+
+		e = e || window.event;
+		const target = e.target;
+		const btn = target.closest('.map__btn');
+
+		if (!btn || btn.classList.contains('map__btn_active')) return;
+		this.querySelector('.map__btn_active').classList.remove('map__btn_active');
+		map.setCenter(window[btn.dataset.city]);
+		btn.classList.add('map__btn_active')
+
+	})
 
 }
 
